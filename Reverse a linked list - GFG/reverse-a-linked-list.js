20fd3c0415e9cfe5012fe88b3b212de08af37c34
Robin Singh <32171406+haddercone@users.noreply.github.com>
2022-10-29 11:58:11 +0530

    Added solution - LeetHub

---
+++ Reverse a linked list - GFG/reverse-a-linked-list.js
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function printlist(head){
+    let current = head; 
+    let s='';
+    while (current !== null)
+    {
+        s+=current.data+" ";
+        current = current.next;
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for(let i=1;i<n;i++){
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let obj = new Solution();
+        head = obj.reverseList(head);
+        printlist(head);
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    //Function to reverse a linked list.
+    reverseList(head)
+    {
+        //your code here
+        let prev = null;
+        let next =  null;
+        let curr = head;
+        
+        if (head === null){
+            return;
+        }
+        else if (head.next === null){
+            return head;
+        }
+        
+        while(curr !== null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+    
+}
