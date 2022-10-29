8403f68acd689c3d36d05825e2e0099c4dc46be5
Robin Singh <32171406+haddercone@users.noreply.github.com>
2022-10-29 13:04:51 +0530

    Added solution - LeetHub

---
+++ Detect Loop in linked list - GFG/detect-loop-in-linked-list.js
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function loopHere(head, tail, position){
+    if(position === 0) 
+        return;
+    let walk = head;
+    for(let i = 1; i < position; i++)
+        walk = walk.next;
+    tail.next = walk;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for(let i=1;i<n;i++){
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let pos = parseInt(readLine());
+        loopHere(head,tail,pos);
+        let obj = new Solution();
+        if(obj.detectLoop(head)){
+            console.log("True");
+        }
+        else{
+            console.log("False");
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {boolean}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    detectLoop(head)
+    {
+        //your code here
+        let slowHead = head;
+        let fastHead = head.next
+        
+        while (slowHead != fastHead){
+            if (fastHead === null || fastHead.next === null){
+                return false;
+            }
+            slowHead = slowHead.next;
+            fastHead = fastHead.next.next
+        }
+        return true;
+        
+    }
+    
+}
