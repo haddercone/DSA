fb428adfcc67dddf759fdfdf1dcc6b556f02dc5a
Robin Singh <32171406+haddercone@users.noreply.github.com>
2022-10-30 11:00:09 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.js
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size)
+{
+    let i;
+    let s='';
+    for (i=0; i < size; i++) {
+        s += arr[i] + " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar0 = readLine().split(' ').map(x=>parseInt(x));
+        let n = input_ar0[0];
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        obj.sort012(arr, n);
+        printArray(arr, arr.length);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number[]}
+*/
+
+class Solution {
+    
+    sort012(arr, N)
+    {
+        //your code here
+        let low = 0;
+        let mid = 0;
+        let hi = N-1
+        
+        if (arr === []){
+            return -1;
+        }
+        
+        while(mid<=hi){
+            if(arr[mid] === 0){
+                [arr[low], arr[mid]] = [arr[mid], arr[low]]
+                low++;
+                mid++;
+            }
+            else if(arr[mid] === 1){
+                mid++;
+            }
+            else{
+                [arr[mid], arr[hi]] = [arr[hi], arr[mid]]
+                hi--;
+            }
+        }
+        return arr;
+        
+    }
+}
