27126efb98de86949ee2ec2c9957882e2626a13a
Robin Singh <32171406+haddercone@users.noreply.github.com>
2022-12-03 12:41:56 +0530

    Added solution - LeetHub

---
+++ Level order traversal - GFG/level-order-traversal.js
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str){
+   // Corner Case
+   if(str.length === 0 || str[0] === "N")
+       return null;
+
+   // Create the root of the tree
+   let root = new Node(parseInt(str[0]));
+
+   // Push the root to the queue
+   let queue = [];
+   let start = 0;
+   queue.push(root);
+
+   // Starting from the second element
+   let i = 1;
+   while(queue.length !== start && i < str.length) {
+
+       // Get and remove the front of the queue
+       let currNode = queue[start];
+       start++;
+
+       // Get the current node's value from the string
+       let currVal = str[i];
+
+       // If the left child is not null
+       if(currVal !== "N") {
+
+           // Create the left child for the current node
+           currNode.left = new Node(parseInt(currVal));
+
+           // Push it to the queue
+           queue.push(currNode.left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= str.length)
+           break;
+       currVal = str[i];
+
+       // If the right child is not null
+       if(currVal !== "N") {
+
+           // Create the right child for the current node
+           currNode.right = new Node(parseInt(currVal));
+
+           // Push it to the queue
+           queue.push(currNode.right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar1 = readLine().split(' ');
+        let root = buildTree(input_ar1);
+        let obj = new Solution();
+        let res = obj.levelOrder(root);
+        let s = '';
+        for(let i=0; i<res.length; i++){
+            s += res[i] +" ";
+        }
+        console.log(s);
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {Node} node
+ * @returns {number[]}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    //Function to return the level order traversal of a tree.
+    levelOrder(node)
+    {
+        if (node === null) return [];
+        
+        let res = []
+        let q = []
+        q.push(node)
+        
+        while(q.length ){
+            
+            let temp = q.shift();
+            res.push(temp.data);
+            
+            if (temp.left) q.push(temp.left);
+            if (temp.right) q.push(temp.right);
+            
+        }
+        return res;
+    }
+}
