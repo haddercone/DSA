276ac11c7f2d57207103c1e36cb2511e907dcba7
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-04-25 21:04:03 +0530

    Time: 195 ms (22.64%), Space: 50.1 MB (52.83%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.js
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.js
@@ -7,6 +7,24 @@ var SmallestInfiniteSet = function() {
  * @return {number}
  */
 SmallestInfiniteSet.prototype.popSmallest = function() {
+    /**
+    Flow of the code: 
+        get index of minimum number;
+        set value at the index to 0. e.g. 
+        return the increamented value;
+    E.g: 
+        For 1: 
+            - the minNumberIndex will be 0
+            - set 0 at that index 0
+            - return minNumberIndex + 1 // 0+1 = 2 --> min value
+        
+        For 2:
+        - the minNumberIndex will be 1 (since the value at index 0 is 0 ( for 1 we replced 1 with 0) , findIndex will return the index of non-zero value i.e index = 1 )
+        - set 0 at at index 1
+        - return minNumberIndex + 1 // 1+1 = 2 --> min value
+        
+    **/
+    
     let minNumberIndex = this.set.findIndex(a => a); // 
     this.set[minNumberIndex] = 0; // 
     return minNumberIndex + 1; // return the index + 1 value
