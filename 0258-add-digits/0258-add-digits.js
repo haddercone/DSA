0ed098cfcd107908f2acd62f29a2f29664278224
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-04-26 12:11:44 +0530

    Time: 78 ms (32.28%), Space: 42.9 MB (99.07%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,9 +3,13 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    let sum = num.toString().split("").reduce((acc, curr) => Number(acc) + Number(curr), 0);
-    if(sum < 10) return sum;
-    return addDigits(sum);
+    // Approach 1
+    // // get digit sum
+    // let sum = num.toString().split("").reduce((acc, curr) => Number(acc) + Number(curr), 0);
+    // // check if sum is less than 0
+    // if(sum < 10) return sum;
+    // return addDigits(sum); // call addDigit again
     
-    
+    // Approach 2:
+    return 1 + (num - 1) % 9;
 };
