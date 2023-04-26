e9593a9cb2ae44407e2fbf1881a0948bad0b78da
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-04-26 14:25:52 +0530

    Time: 105 ms (51.74%), Space: 48.3 MB (15.43%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,51 +11,28 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let newList = new ListNode();
-    let ptr = newList;
-    let sum = 0;
-    if(l1 === null && l2 === null){
-        return 0;
-    }
-    if(l1 === null){
-        return l2
-    }
-    if(l2 === null){
-       return l1
-    }
+    let newList = new ListNode(); 
+    let ptr = newList; 
+    let sum = 0; 
     
+    if(!l1 && !l2) return null;
     while(l1 || l2){
-        if(l1 && l2){
-            sum += l1.val + l2.val;
-        } else if(l1){
-            sum+=l1.val;
-        } else {
-            sum+=l2.val;
-        }
-        
+        sum =  (l1?.val ?? 0) + (l2?.val ?? 0) + sum; 
         if(sum > 9){
             let rem = sum%10;
-            newList.next = new ListNode(rem);
+            newList.next = new ListNode(rem); 
         } else {
-            newList.next = new ListNode(sum);
+            newList.next = new ListNode(sum); 
         }
-        
         sum = parseInt(sum/10);
         newList = newList.next;
-        
-        if(l1 && l2){
-            l1 = l1.next;
-            l2 = l2.next;
-        } else if(l1){
-            l1 = l1.next
-        } else {
-            l2 = l2.next;
-        }
-
+        l1 = l1?.next;
+        l2 = l2?.next;
     }
+
     if(sum !== 0){
         newList.next = new ListNode(sum)
         newList = newList.next;
     }
-    return ptr.next;
+    return ptr.next; 
 };
