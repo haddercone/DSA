7db660607476de4c5a917b9700a568434385c053
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-04-29 19:03:58 +0530

    Time: 74 ms (80.47%), Space: 43 MB (77.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -6,14 +6,18 @@
 var isAnagram = function(s, t) {
 
     let map = {};
+    
     if(s.length != t.length) return false;
+    
+    // create a map containing character frequency in s
     for(let i =0; i< s.length;i++){
-        if(map[s[i]]) map[s[i]]+=1;
+        if(map[s[i]]) map[s[i]]++;
         else{
             map[s[i]] = 1;
         }
     }
-
+    
+    // reduce the character frequecy , when character is encountered;
     for(let j =0; j<t.length;j++){
         if(map[t[j]]) map[t[j]]--;
     }
