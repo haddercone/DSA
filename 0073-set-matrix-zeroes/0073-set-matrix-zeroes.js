991951b6240bd87071a4b41c6e0ec3e2ca18ec3f
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-06-13 19:24:10 +0530

    Time: 93 ms (18.64%), Space: 45.4 MB (22.04%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let rows = [];
+    let cols = [];
+    let m = matrix.length;
+    let n = matrix[0].length;
+    // get indexes of 0s
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(matrix[i][j] === 0){
+                rows.push(i)
+                cols.push(j)
+            }
+        }
+    }
+    
+    while(rows.length > 0){
+        let row = rows.shift();
+        matrix[row] = matrix[row].fill(0);
+    }
+    
+    while(cols.length > 0){
+        let col = cols.shift();
+        for(let i=0;i < m;i++){
+            matrix[i][col] = 0;
+        }
+    }
+    
+    return matrix;
+};
