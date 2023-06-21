4070211eac6a44d2b086bdf677dedfb9e1fab640
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-06-21 21:08:28 +0530

    Time: 304 ms (15.77%), Space: 49.1 MB (22.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,22 +3,47 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s.length < 1) return s.length;
-    let maxCount = 1;
+    // SC = O(n)
+    // TC =  O(n)
+    const map = new Map();
+    let string = "";
+    let maxLength=0;
     for(let i=0;i<s.length;i++){
-        let count = 1;
-        let set = new Set();
-        for(let j=i+1;j<s.length;j++){
-            if(s[j] === s[i] || set.has(s[j])) break;
-            set.add(s[j])
-            count++;
-            if(count >= maxCount){
-                maxCount = count;
-            }            
+        let char = s[i];
+        if(!map.has(char)){
+            map.set(char,i);
+            string+=s[i];
+            if(string.length > maxLength){
+                maxLength = string.length;
+            }
+        } else {
+            i = map.get(char);
+            string="";
+            map.clear();
         }
-        delete set;
     }
-    return maxCount;
+    return maxLength;
+    
+    
+    // SC = O(k), k=numberof unique characters in string
+    // TC =  O(n^2)
+    
+//     if(s.length < 1) return s.length;
+//     let maxCount = 1;
+//     for(let i=0;i<s.length;i++){
+//         let count = 1;
+//         let set = new Set();
+//         for(let j=i+1;j<s.length;j++){
+//             if(s[j] === s[i] || set.has(s[j])) break;
+//             set.add(s[j])
+//             count++;
+//             if(count >= maxCount){
+//                 maxCount = count;
+//             }            
+//         }
+//         delete set;
+//     }
+//     return maxCount;
     
     
     
