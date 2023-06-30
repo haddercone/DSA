e00f4aa16dfe97df471775858d98e076c9a99f77
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-06-30 20:29:54 +0530

    Time: 43 ms (99.67%), Space: 41.8 MB (91.51%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -5,15 +5,26 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
     let maxNum = Math.max(...candies);
-    const res = [];
     
-    for(let i=0;i<candies.length;i++){
-        let maxCandies = candies[i] + extraCandies;
-        if(maxCandies >= maxNum){
-            res.push(true)
-        } else {
-            res.push(false)
-        }
-    }
+    // Approch 2 : using reduce
+    const res = candies.reduce((acc, curr) => {
+        let bool = (curr + extraCandies) >= maxNum ? true : false;
+        acc.push(bool);
+        return acc;
+    }, []);
+    
     return res;
+    
+    // Approch 1 : using loop
+//     const res = [];
+    
+//     for(let i=0;i<candies.length;i++){
+//         let maxCandies = candies[i] + extraCandies;
+//         if(maxCandies >= maxNum){
+//             res.push(true)
+//         } else {
+//             res.push(false)
+//         }
+//     }
+//     return res;
 };
