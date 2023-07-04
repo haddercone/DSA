7af5e36cd0ae557eddbfec461d2fa28fcf1e39ca
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-07-04 21:25:42 +0530

    Time: 105 ms (32.52%), Space: 51.8 MB (64.76%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -5,13 +5,22 @@
  */
 var rotate = function(nums, k) {
     const n = nums.length;
-    if(k>n){
-        k = k %n;
-    }
-    const subArr = nums.splice(-k);
+//     if(k>n){
+//         k = k % n; // 
+//     }
+//     const subArr = nums.splice(-k); // get last k elements
     
-    for(let i=subArr.length-1;i>=0;i--){
-        nums.unshift(subArr[i])
+//     for(let i=subArr.length-1;i>=0;i--){
+//         nums.unshift(subArr[i])
+//     }
+//     return nums;
+    
+    const temp = new Array(n).fill(0);
+    for(let i=0;i<n;i++){
+        temp[(i+k)%n] = nums[i]
+    }
+    for(let j=0;j<n;j++){
+        nums[j] = temp[j]
     }
     return nums;
 };
