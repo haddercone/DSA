743edb5cf9f8d018a82b0e64a18882148ef3c51b
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-07-08 19:43:48 +0530

    Time: 63 ms (79.96%), Space: 45.2 MB (51.07%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,21 +3,28 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    // nums.sort((a,b) => a-b);
-    let map = new Map();
-    for(let i=0;i<nums.length;i++){
-        if(!map.has(nums[i])){
-            map.set(nums[i], 1)
-        } else {
-            let val = map.get(nums[i]);
-            val++;
-            map.set(nums[i], val)
+    nums.sort((a,b) => a-b);
+    for(let i=1;i<nums.length;i+=2){
+        if(nums[i] !== nums[i-1]){
+            return nums[i-1]
         }
+    }
+     return nums[nums.length -1]
+//     let map = new Map();
+//     for(let i=0;i<nums.length;i++){
+//         if(!map.has(nums[i])){
+//             map.set(nums[i], 1)
+//         } else {
+//             let val = map.get(nums[i]);
+//             val++;
+//             map.set(nums[i], val)
+//         }
         
-    }
-    for(let i=0;i<nums.length;i++){
-        if(map.get(nums[i]) === 1){
-            return nums[i];
-        }
-    }
+//     }
+    
+//     for(let i=0;i<nums.length;i++){
+//         if(map.get(nums[i]) === 1){
+//             return nums[i];
+//         }
+//     }
 };
