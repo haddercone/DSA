288b9d02af47119d65f9b5d717bf6b41b71e196d
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-07-09 15:01:08 +0530

    Time: 87 ms (27.05%), Space: 48.2 MB (16.97%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    return nums.reduce((acc,curr) => acc^curr, 0) // using bitwise XOR
+    // return nums.reduce((acc,curr) => acc^curr, 0) // using bitwise XOR
     
     
     // const uniquenumberSum = [...new Set(nums)].reduce((acc,curr) => acc+curr, 0);
@@ -20,7 +20,15 @@ var singleNumber = function(nums) {
     //  return nums[nums.length -1]
     
     
-    
+    const map ={}
+    for(num of nums){
+        map[num] = (map[num] || 0) + 1
+    }
+    for(const key in map){
+        if(map[key] === 1){
+            return key;
+        }
+    }
     
 //     let map = new Map();
 //     for(let i=0;i<nums.length;i++){
@@ -31,9 +39,7 @@ var singleNumber = function(nums) {
 //             val++;
 //             map.set(nums[i], val)
 //         }
-        
 //     }
-    
 //     for(let i=0;i<nums.length;i++){
 //         if(map.get(nums[i]) === 1){
 //             return nums[i];
