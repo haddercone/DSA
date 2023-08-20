ecec9812846792dc8541c4477fd16e0e78c48cac
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-08-20 17:37:05 +0530

    Time: 60 ms (64.44%), Space: 42.5 MB (26.73%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -5,12 +5,19 @@
  */
 var arithmeticTriplets = function(nums, diff) {
     let count = 0
-    for(let i=0;i<nums.length;i++){
-        for(let j=i;j<nums.length;j++){
-            for(let k=j;k<nums.length;k++){
-                if(nums[k]-nums[j] === diff && nums[j]-nums[i] === diff) count++;
-            }
-        }
+    
+    let set = new Set();
+    for(const num of nums){
+        if(set.has(num-diff) && set.has(num-2*diff)) count++;
+        set.add(num)
     }
+    
+    // for(let i=0;i<nums.length;i++){
+    //     for(let j=i;j<nums.length;j++){
+    //         for(let k=j;k<nums.length;k++){
+    //             if(nums[k]-nums[j] === diff && nums[j]-nums[i] === diff) count++;
+    //         }
+    //     }
+    // }
     return count;
 };
