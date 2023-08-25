5c27fd2d27d4c7df8874cfa4cbbd5ecb81392314
Robin Singh <32171406+haddercone@users.noreply.github.com>
2023-08-25 14:52:36 +0530

    Time: 54 ms (62.50%), Space: 41.5 MB (86.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,8 +3,19 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let BigNum = BigInt(digits.join(""))
-    let sum = BigNum + 1n;
-    return String(sum).split("")
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i] < 9){
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits = new Array(digits.length + 1).fill(0)
+    digits[0] = 1
+    return digits;
+    
+    // let BigNum = BigInt(digits.join(""))
+    // let sum = BigNum + 1n;
+    // return String(sum).split("")
     
 };
