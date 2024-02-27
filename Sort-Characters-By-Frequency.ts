a751d48eb6ae493a113819cab8aecba3c108a506
Robin Singh <32171406+haddercone@users.noreply.github.com>
2024-02-27 19:50:46 +0530

    Sort-Characters-By-Frequency.ts

---
+++ Sort-Characters-By-Frequency.ts
@@ -0,0 +1,13 @@
+function frequencySort(s: string): string {
+    const count: { [key: string]: number } = {};
+    for (const char of s) {
+        count[char] = count[char] + 1 || 1;
+    }
+
+    const sortedByFrequencies : string = Object.entries(count)
+                                            .sort((a,b) => b[1] - a[1])
+                                            .map(ele => ele[0]
+                                            .repeat(ele[1])).join("");
+    return sortedByFrequencies;
+
+};
